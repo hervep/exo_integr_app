8b4069dbe590cc495db7916854b2b77ad44e6a0c
DJakou herve <172897@umons.ac.be>
2019-05-15 18:10:05 +0200

    first

---
+++ app/src/main/java/com/example/exercice_integre/ActionViewActivity.java
@@ -0,0 +1,56 @@
+package com.example.exercice_integre;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class ActionViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.action_view);
+
+        final ProjetBDD actionBDD = new ProjetBDD(this);
+
+        EditText display = findViewById(R.id.display);
+        EditText display2 = findViewById(R.id.display2);
+
+        Button backButton = findViewById(R.id.backButton);
+
+
+        actionBDD.openForRead();
+        ArrayList<Projet> projectList = actionBDD.getAllproject();
+
+
+        display.setText(projectList.toString());
+        display2.setText(actionBDD.somme());
+
+
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ActionViewActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/exercice_integre/UpdateActivity.java
@@ -0,0 +1,80 @@
+package com.example.exercice_integre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update);
+
+        final ProjetBDD actionBDD = new ProjetBDD(this);
+
+        final EditText id_update = findViewById(R.id.id_update);
+        final EditText action_update = findViewById(R.id.action_update);
+        final EditText duration_update = findViewById(R.id.duration_update);
+        final EditText date_update = findViewById(R.id.date_update);
+
+
+        Button backButton = findViewById(R.id.backButton);
+        Button updateButton = findViewById(R.id.updateButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(UpdateActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = Integer.valueOf(id_update.getText().toString());
+                String action = action_update.getText().toString();
+                String date = date_update.getText().toString();
+                int duration = Integer.valueOf(duration_update.getText().toString());
+
+
+
+                Log.d(" texte", action_update.getText().toString());
+                Log.d(" texte", date_update.getText().toString());
+                Log.d(" texte", duration_update.getText().toString());
+                Log.d(" texte", id_update.getText().toString());
+
+                Projet projet = new Projet();
+                projet.setDuree(duration);
+                projet.setDate(date);
+                projet.setAction(action);
+
+
+                actionBDD.openForWrite();
+                actionBDD.updateProject(id,projet);
+                actionBDD.close();
+
+                id_update.setText("");
+                action_update.setText("");
+                date_update.setText("");
+                duration_update.setText("");
+
+
+                Toast.makeText(getApplicationContext(),"Done",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
